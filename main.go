7282af101e1b59c49fc98a5ceb6a6223e3b4e4fe
theaaronmartin <grav3serker@gmail.com>
2024-11-02 19:21:30 -0700

    restructuring and adding route for games

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-// KEY: D9E597D88058F1EFA44434531CC57183
-// https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=D9E597D88058F1EFA44434531CC57183&steamid=76561198096213883&include_appinfo=true&format=json
-// https://cdn.cloudflare.steamstatic.com/steam/apps/3147870/library_600x900_2x.jpg
-
 import (
 	"embed"
 	"encoding/json"
@@ -12,9 +8,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
-//go:embed views/*
+//go:embed views
 var views embed.FS
 
 var t = template.Must(template.ParseFS(views, "views/*"))
@@ -22,7 +19,7 @@ var t = template.Must(template.ParseFS(views, "views/*"))
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		content, err := os.ReadFile("./games.json")
+		content, err := os.ReadFile("./static/json/games.json")
 		if err != nil {
 			log.Fatal("Error when opening file: ", err)
 		}
@@ -32,16 +29,36 @@ func main() {
 		if err != nil {
 			log.Fatal("Error during Unmarshal(): ", err)
 		}
-		if err := t.ExecuteTemplate(w, "index.html", data); err != nil {
+		if err := t.ExecuteTemplate(w, "index.gohtml", data); err != nil {
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		}
+	})
+
+	router.HandleFunc("GET /{id}", func(w http.ResponseWriter, req *http.Request) {
+		content, err := os.ReadFile("./static/json/games.json")
+		if err != nil {
+			log.Fatal("Error when opening file: ", err)
+		}
+		idString := req.PathValue("id")
+		num, err := strconv.Atoi(idString)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		data := Games{}
+		err = json.Unmarshal(content, &data)
+		if err != nil {
+			log.Fatal("Error during Unmarshal(): ", err)
+		}
+		if err := t.ExecuteTemplate(w, "game.gohtml", data.Game[num-1]); err != nil {
 			http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		}
 	})
 
 	server := http.Server{
-		Addr:    ":80",
+		Addr:    ":8000",
 		Handler: router,
 	}
 
-	fmt.Println("Listening on 8000")
 	server.ListenAndServe()
 }
--- types.go
+++ types.go
@@ -1,26 +1,15 @@
 package main
 
-// type Steam struct {
-// 	Count int    `json:"game_count"`
-// 	Games []Game `json:"games"`
-// }
-
-// type Game struct {
-// 	AppId int    `json:"appid"`
-// 	Name  string `json:"name"`
-// }
-
-// type Response struct {
-// 	Steam Steam `json:"response"`
-// }
-
 type Games struct {
 	Game []Game `json:"games"`
 }
 
 type Game struct {
+	Id          int    `json:"id"`
 	Name        string `json:"name"`
 	BoxArt      string `json:"boxArt"`
 	Link        string `json:"link"`
 	IsCompleted bool   `json:"isCompleted"`
+	Date        string `json:"completedOn"`
+	Rating      int    `json:"rating"`
 }
